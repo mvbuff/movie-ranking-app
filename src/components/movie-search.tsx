e2c15d5ea91f18436d87449ba542f3628dd49971
Movie <moviebuff@duck.com>
2025-06-15 16:44:47 -0700

    fix: remove unused variables

--- src/components/movie-search.tsx
+++ src/components/movie-search.tsx
@@ -26,9 +26,6 @@ interface MovieSearchProps {
   onItemAdded: () => void;
 }
 
-const TMDB_API_URL = 'https://api.themoviedb.org/3';
-const TMDB_API_KEY = process.env.NEXT_PUBLIC_TMDB_API_KEY;
-
 export default function MovieSearch({ onItemAdded }: MovieSearchProps) {
   const [query, setQuery] = useState('');
   const [results, setResults] = useState<SearchResult[]>([]);
