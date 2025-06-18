7b59c357b01a3c8def1ba7ea7ead6b9b44d99780
Movie <moviebuff@duck.com>
2025-06-17 17:12:15 -0700

    feat: create dedicated registration flow

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,18 +1,16 @@
 export { default } from "next-auth/middleware";
 
 export const config = {
-  // Matcher to protect all routes except for the ones required for authentication
-  // and user registration.
+  // Matcher to protect all routes except for the ones starting with /api,
+  // login, register, etc.
   matcher: [
     /*
      * Match all request paths except for the ones starting with:
-     * - api/auth (NextAuth.js authentication routes)
-     * - login (the login page)
-     * - register (the registration page)
+     * - api (API routes)
      * - _next/static (static files)
      * - _next/image (image optimization files)
      * - favicon.ico (favicon file)
      */
-    '/((?!api/auth|login|register|_next/static|_next/image|favicon.ico).*)',
+    '/((?!api|login|register|_next/static|_next/image|favicon.ico).*)',
   ],
 }; 
